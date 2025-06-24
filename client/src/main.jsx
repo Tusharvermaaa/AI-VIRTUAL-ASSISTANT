139bfd09307aa8197227a4920f0102077a42c8f2
Tusharvermaaa <tusharverma8433@gmail.com>
2025-06-25 01:31:56 +0530

    resolving importing issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Usercontext from './context/usercontext.jsx'
+// import Usercontext from './context/usercontext.jsx'
+import Usercontext from '../context/usercontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
