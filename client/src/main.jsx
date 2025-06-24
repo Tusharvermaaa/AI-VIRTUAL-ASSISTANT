8de5f016ae022e91d8d627c3450bad6b6ac7f33d
Tusharvermaaa <tusharverma8433@gmail.com>
2025-06-25 02:01:43 +0530

    -

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Usercontext from './context/usercontext.jsx'
+import Usercontext from './context/Usercontext.jsx'
+// import Usercontext from './context/Usercontext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
