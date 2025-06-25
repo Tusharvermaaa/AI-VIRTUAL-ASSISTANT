b44e65f5760960a43c4ce020f1a333815d90b5d0
Tusharvermaaa <tusharverma8433@gmail.com>
2025-06-26 00:12:59 +0530

    cookies error resolving

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,81 +1,89 @@
 import userModel from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import {genToken} from "../config/token.js";
+import { genToken } from "../config/token.js";
 
+export async function signup_auth(req, res) {
+  // console.log("authentaication working , please be prepared ");
+  // console.log(req.body , "inside signup auth controller auth.controller ");
+  const { name, password, email } = req.body;
+  // console.log(req.body , " inside the authcontroller dignup functiom");
+  const user = await userModel.findOne({ email });
+  if (user)
+    return res.json({
+      status: "user already exists in the system  auth controller ",
+    });
+  if (password.length < 3)
+    return res.json({ status: "password length less then 3 auth controller" });
+  const hashedpassword = await bcrypt.hash(password, Number(process.env.SALT));
+  const createduser = await userModel.create({
+    name,
+    password: hashedpassword,
+    email,
+  });
 
-export async function signup_auth(req, res)
-{
-    // console.log("authentaication working , please be prepared ");
-    // console.log(req.body , "inside signup auth controller auth.controller ");
-    const { name , password, email}=req.body;
-    // console.log(req.body , " inside the authcontroller dignup functiom");
-    const user=await userModel.findOne({email});
-    if(user) return res.json({"status" : "user already exists in the system  auth controller "});
-    if(password.length<3) return res.json({"status": "password length less then 3 auth controller"});
-    const hashedpassword= await bcrypt.hash(password,Number( process.env.SALT));
-    const createduser=await userModel.create({name , password:hashedpassword, email});
-
-   const token=genToken(createduser._id );
-//    console.log("token created at authentication and type " , token , typeof token)
-    // console.log("created user id , in signup .jsx in controller " , createduser._id);
-   res.cookie("token" , token , {
-    sameSite:"none" , 
-    secure: true, 
-    httpOnly:true,
-    maxAge:4*24*60*60*1000
-   })
-   return res.status(201).send(createduser);
+  const token = genToken(createduser._id);
+  //    console.log("token created at authentication and type " , token , typeof token)
+  // console.log("created user id , in signup .jsx in controller " , createduser._id);
+  res.cookie("token", token, {
+    sameSite: "none",
+    secure: true,
+    httpOnly: true,
+    maxAge: 4 * 24 * 60 * 60 * 1000,
+  });
+  return res.status(201).send(createduser);
 }
 // login authentication
 export async function login_auth(req, res) {
-    try {
-        // console.log("login handling server, req.body is", req.body);
-        const { email, password } = req.body;
-        
-        // Validate input
-        if (!email || !password) {
-            return res.status(400).json({ message: "Email and password are required" });
-        }
+  try {
+    // console.log("login handling server, req.body is", req.body);
+    const { email, password } = req.body;
 
-        // Find user with password (must await this query!)
-        const user = await userModel.findOne({ email }).select("+password");
-        
-        if (!user) {
-            console.log("user does not exist");
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        // Compare passwords
-        const pmatch = await bcrypt.compare(password, user.password);
-        if (!pmatch) {
-            console.log("password does not match");
-            return res.status(401).json({ message: "Incorrect password" });
-        }
-
-        // console.log("generating token");
-        const token = genToken(user._id, email);
-        
-        // Set cookie
-        res.cookie("token", token, {
-            sameSite: "strict",
-            secure: false,
-            httpOnly: true,
-            maxAge: 4 * 24 * 60 * 60 * 1000
-        });
-
-        // console.log("returning the response");
-        return res.status(200).json({
-            status: "Login successful",
-            user: {
-                id: user._id,
-                email: user.email
-                // Add other non-sensitive user fields here
-            }
-        });
-    } catch (error) {
-        console.log("Login error:", error);
-        return res.status(500).json({ message: "Internal server error" });
+    // Validate input
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
     }
+
+    // Find user with password (must await this query!)
+    const user = await userModel.findOne({ email }).select("+password");
+
+    if (!user) {
+      console.log("user does not exist");
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Compare passwords
+    const pmatch = await bcrypt.compare(password, user.password);
+    if (!pmatch) {
+      console.log("password does not match");
+      return res.status(401).json({ message: "Incorrect password" });
+    }
+
+    // console.log("generating token");
+    const token = genToken(user._id, email);
+
+    // Set cookie
+    res.cookie("token", token, {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+      maxAge: 4 * 24 * 60 * 60 * 1000,
+    });
+
+    // console.log("returning the response");
+    return res.status(200).json({
+      status: "Login successful",
+      user: {
+        id: user._id,
+        email: user.email,
+        // Add other non-sensitive user fields here
+      },
+    });
+  } catch (error) {
+    console.log("Login error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 }
 /*
 export async function login_auth(req, res)
@@ -114,20 +122,18 @@ export async function login_auth(req, res)
     // return res.status(200).send(user);
 }
 */
-//logout 
+//logout
 
-export async function logout_auth(req, res)
-{
-    try{
-
-        res.clearCookie("token");
-        // return res.redirect("/signin");
-        return res.status(200).send({"message": "logout successfully from auth.controller"})
-    }
-    catch(err)
-    {
-        return res.status(400).send({"message":"logout error  ,  auth controler"});
-    }
+export async function logout_auth(req, res) {
+  try {
+    res.clearCookie("token");
+    // return res.redirect("/signin");
+    return res
+      .status(200)
+      .send({ message: "logout successfully from auth.controller" });
+  } catch (err) {
+    return res.status(400).send({ message: "logout error  ,  auth controler" });
+  }
 }
 
 // export default {signup_auth , login_auth , logout_auth};
