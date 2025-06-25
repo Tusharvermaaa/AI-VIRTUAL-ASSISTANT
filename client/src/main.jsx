06da87c49291736eb9aafc338d3cb547339fbf12
Tusharvermaaa <tusharverma8433@gmail.com>
2025-06-25 23:12:50 +0530

    main.jsx editing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { userdatacontext } from "./context/Usercontext.jsx";
-
 import Usercontext from "./context/Usercontext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <userdatacontext>
+    <Usercontext>
       <App />
-    </userdatacontext>
+    </Usercontext>
   </BrowserRouter>
 );
